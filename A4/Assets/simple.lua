4aa7ea4be06e638fcc20f4270a72aa3fdae28788
David Su <dsu1995@yahoo.ca>
2017-06-21 00:30:31 -0400

    custom scene

--- A4/Assets/simple.lua
+++ A4/Assets/simple.lua
@@ -26,6 +26,62 @@ s5 = gr.nh_sphere('s5', {0, 100, -250}, 25)
 scene_root:add_child(s5)
 s5:set_material(mat1)
 
+mickey = gr.mesh('mickey', 'mickey.obj')
+scene_root:add_child(mickey)
+mickey:set_material(mat3)
+mickey:scale(250, 250, 250)
+mickey:rotate('x', -90)
+mickey:rotate('y', -90)
+mickey:translate(-200, -200, 0)
+
+steldodec = gr.mesh( 'dodec', 'smstdodeca.obj' )
+steldodec:set_material(mat3)
+scene_root:add_child(steldodec)
+
+
+cow_poly = gr.mesh('cow', 'cow.obj')
+factor = 20
+hide = gr.material({0.84, 0.6, 0.53}, {0.3, 0.3, 0.3}, 20)
+cow_poly:set_material(hide)
+cow_poly:translate(0.0, 3.637, 0.0)
+cow_poly:scale(factor, factor, factor)
+cow_poly:translate(-250.0, 0.0, 0.0)
+scene_root:add_child(cow_poly)
+
+buckyball = gr.mesh( 'buckyball', 'buckyball.obj' )
+scene_root:add_child(buckyball)
+stone = gr.material({0.8, 0.7, 0.7}, {0.0, 0.0, 0.0}, 0)
+buckyball:set_material(stone)
+buckyball:scale(50, 50, 50)
+buckyball:translate(200, -200, 0)
+
+-- the arc
+gold = gr.material({0.9, 0.8, 0.4}, {0.8, 0.8, 0.4}, 25)
+
+arc = gr.node('arc')
+scene_root:add_child(arc)
+arc:scale(30, 30, 30)
+arc:rotate('Y', 30)
+arc:translate(250,-50,50)
+p1 = gr.cube('p1')
+arc:add_child(p1)
+p1:set_material(gold)
+p1:scale(0.8, 4, 0.8)
+p1:translate(-2.4, 0, -0.4)
+
+p2 = gr.cube('p2')
+arc:add_child(p2)
+p2:set_material(gold)
+p2:scale(0.8, 4, 0.8)
+p2:translate(1.6, 0, -0.4)
+
+s = gr.sphere('s')
+arc:add_child(s)
+s:set_material(gold)
+s:scale(4, 0.6, 0.6)
+s:translate(0, 4, 0)
+
+
 white_light = gr.light({-100.0, 150.0, 400.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
 orange_light = gr.light({400.0, 100.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
 
